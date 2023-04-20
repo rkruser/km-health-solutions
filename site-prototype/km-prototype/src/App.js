56a4982bc42e2e573047726c75a7748effa7f95b
Ryen Krusinga <krusinga@umiacs.umd.edu>
2023-04-20 14:27:16 -0400

    Added text inputs with gpt4

--- site-prototype/km-prototype/src/App.js
+++ site-prototype/km-prototype/src/App.js
@@ -1,29 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
       <body>
-        <h1>Hello world</h1>
-        <p>Here is some paragraph text</p>
+        <DoubleInputArea />
       </body>
     </div>
   );
 }
 
 export default App;
+
+// ****************** GPT-4 Code (with some modifications) *******************
+function DoubleInputArea() {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+
+  const handleSubmit1 = () => {
+    // Add your custom logic here
+    console.log('Submit1:', input1);
+  };
+
+  const handleSubmit2 = () => {
+    // Add your custom logic here
+    console.log('Submit2:', input2);
+  };
+
+  return (
+    <div className="container">
+      <div className="text-input-container">
+        <textarea
+          className="text-input"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+          placeholder="Enter text here..."
+        />
+        <button className="submit-btn" onClick={handleSubmit1}>
+          Submit
+        </button>
+      </div>
+      <div className="text-input-container">
+        <textarea
+          className="text-input"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+          placeholder="Enter text here..."
+        />
+        <button className="submit-btn" onClick={handleSubmit2}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+
