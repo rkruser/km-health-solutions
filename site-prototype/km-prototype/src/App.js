a19b345c42ebd16589663ea7e9fcb8bdf8690e4a
Ryen Krusinga <krusinga@umiacs.umd.edu>
2023-04-21 10:45:22 -0400

    Can display responses on the page

--- site-prototype/km-prototype/src/App.js
+++ site-prototype/km-prototype/src/App.js
@@ -20,14 +20,16 @@ export default App;
 function DoubleInputArea() {
   const [input1, setInput1] = useState('');
   const [input2, setInput2] = useState('');
+  const [response1, setResponse1] = useState('');
+  const [response2, setResponse2] = useState('');
 
   const handleSubmit1 = () => {
-    // Add your custom logic here
     console.log('Submit1:', input1);
 
     completeText(input1)
       .then((completion) => {
         console.log('Completion:', completion);
+        setResponse1(completion);
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -35,18 +37,18 @@ function DoubleInputArea() {
   };
 
   const handleSubmit2 = () => {
-    // Add your custom logic here
     console.log('Submit2:', input2);
 
     let pre_prompt = "You are a chatbot proxy for someone named Ryen Krusinga. Sign off all your responses with --Ryen K.\n\n";
 
     completeText(pre_prompt + input2)
-    .then((completion) => {
-      console.log('Completion:', completion);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+      .then((completion) => {
+        console.log('Completion:', completion);
+        setResponse2(completion);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
   return (
@@ -61,6 +63,9 @@ function DoubleInputArea() {
         <button className="submit-btn" onClick={handleSubmit1}>
           Submit
         </button>
+        <div className="response">
+          {response1}
+        </div>
       </div>
       <div className="text-input-container">
         <textarea
@@ -72,10 +77,13 @@ function DoubleInputArea() {
         <button className="submit-btn" onClick={handleSubmit2}>
           Submit
         </button>
+        <div className="response">
+          {response2}
+        </div>
       </div>
     </div>
   );
-};
+}
 
 
 
