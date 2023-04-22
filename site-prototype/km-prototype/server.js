bd7bb73c1ba7dc85f367dddf6becfc841a928292
Ryen Krusinga <krusinga@umiacs.umd.edu>
2023-04-21 21:51:26 -0400

    Added password protection

--- site-prototype/km-prototype/server.js
+++ site-prototype/km-prototype/server.js
@@ -13,17 +13,24 @@ const credentials = {
   cert: certificate,
 };
 
-app.use(express.static('build')); // Replace 'build' with the path to your React app's build directory
+// Set up Basic Authentication
+const basicAuth = require('express-basic-auth');
+const users = { 'admin': 'TTd3#U#KmY=na>)QAk?p' }; // Replace with your desired username and password
 
+app.use(basicAuth({
+  users: users,
+  challenge: true,
+  unauthorizedResponse: 'Unauthorized',
+}));
+
+app.use(express.static('build')); // Replace 'build' with the path to your React app's build directory
 
 const server = https.createServer(credentials, app).listen(port, () => {
   console.log(`Server running at https://localhost:${port}`);
 });
 
-
 server.on('request', (req, res) => {
-    console.log(`Incoming request from ${req.connection.remoteAddress}:${req.connection.remotePort}`);
-    console.log(`Request method: ${req.method}`);
-    console.log(`Request URL: ${req.url}`);
-  });
-  
+  console.log(`Incoming request from ${req.connection.remoteAddress}:${req.connection.remotePort}`);
+  console.log(`Request method: ${req.method}`);
+  console.log(`Request URL: ${req.url}`);
+});
