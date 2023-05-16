3d1667dcdeafd8ea9988b33d9faa9103447498cf
Ryen Krusinga <krusinga@umiacs.umd.edu>
2023-05-16 18:13:29 -0400

    Can now run basic react component

--- frontend/site-frontend/src/App.js
+++ frontend/site-frontend/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React, { useState } from 'react';
+import {Helmet} from 'react-helmet';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <title>Demo medical app</title>
+      </Helmet>
+      <div className="site-banner">
+        <h1 className="site-title">Demo Medical App</h1>
+      </div>
+      <div>
+        <PatientInput />
+      </div>
+      <div style={{height: "40px"}}>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+function generateData(text) {
+  return text+" hello"
+}
+
+
+function PatientInput() {
+  const [inputText, setInputText] = useState('');
+  const [displayText, setDisplayText] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setDisplayText(generateData(inputText));
+  };
+
+  return (
+    <div className="PatientInput">
+      <div className="input-pane">
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          className="text-entry"
+        />
+        <button onClick={handleSubmit} className="generate-button">
+          Generate
+        </button>
+      </div>
+      <div className="display-pane">
+        <p>{displayText}</p>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+
