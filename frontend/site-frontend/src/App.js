5299a69104c5e58ccacbae7873673fe3942bcd4a
Ryen Krusinga <krusinga@umiacs.umd.edu>
2023-05-16 19:09:09 -0400

    Added AI chat window

--- frontend/site-frontend/src/App.js
+++ frontend/site-frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {Helmet} from 'react-helmet';
 
 
@@ -15,6 +15,7 @@ function App() {
       </div>
       <div>
         <PatientInput />
+        <AIchat />
       </div>
       <div style={{height: "40px"}}>
       </div>
@@ -26,8 +27,9 @@ export default App;
 
 
 function generateData(text) {
-  return text+" hello"
+  return text.toUpperCase() + " hello";
 }
+
 
 
 function PatientInput() {
@@ -61,6 +63,75 @@ function PatientInput() {
   );
 }
 
+function chatWithAI(text) {
+  return "placeholder text!!"
+}
+
+function performAction(action) {
+  return "placeholder text! 2222"
+}
+
+function AIchat() {
+  const [inputText, setInputText] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+  const chatWindowRef = useRef(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatHistory]);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const reply = await chatWithAI(inputText);
+    addMessageToChat(`User: ${inputText}`);
+    addMessageToChat(`AI: ${reply}`);
+    setInputText('');
+  };
+
+  const addMessageToChat = (message) => {
+    setChatHistory((prevChatHistory) => [...prevChatHistory, message]);
+  };
+
+  const handleButtonClick = async (action) => {
+    const result = await performAction(action);
+    addMessageToChat(`AI: ${result}`);
+  };
+
+  const scrollToBottom = () => {
+    if (chatWindowRef.current) {
+      chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
+    }
+  };
+
+  return (
+    <div className="AIchat">
+      <div className="left-column">
+        <button onClick={() => handleButtonClick('summarize')}>Summarize</button>
+        <button onClick={() => handleButtonClick('keywords')}>Keywords</button>
+        <button onClick={() => handleButtonClick('format as')}>Format As</button>
+      </div>
+      <div className="chat-container">
+        <div className="chat-window" ref={chatWindowRef}>
+          {chatHistory.map((message, index) => (
+            <p key={index}>{message}</p>
+          ))}
+        </div>
+        <div className="chat-input">
+          <input
+            type="text"
+            value={inputText}
+            onChange={handleInputChange}
+            placeholder="Type your message..."
+          />
+          <button onClick={handleSubmit}>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 
 
