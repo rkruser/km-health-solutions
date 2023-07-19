ed1767359d67f3d447d29d4e5d3344c23f7e2788
Ryen Krusinga <krusinga@umiacs.umd.edu>
2023-07-19 18:49:43 -0400

    Fixed app url for packaged project

--- frontend/desktop-frontend/demo-app/electron_src/main.ts
+++ frontend/desktop-frontend/demo-app/electron_src/main.ts
@@ -28,7 +28,7 @@ function createWindow() {
   
   const appURL = app.isPackaged
     ? url.format({
-        pathname: path.join(__dirname, "./build/index.html"),
+        pathname: path.join(__dirname, "../build/index.html"),
         protocol: "file",
         slashes: true,
         })
