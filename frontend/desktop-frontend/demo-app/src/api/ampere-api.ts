634007b4e2774318abb20f2bb46710c0340df866
rkruser <rkrusinga@gmail.com>
2023-08-07 13:49:24 -0400

    Saving state before merge

---
+++ frontend/desktop-frontend/demo-app/src/api/ampere-api.ts
@@ -0,0 +1,16 @@
+/*
+
+Designing the API:
+
+Three part design:
+1. Top level: a functional abstraction
+2. Mid level: an API interface implementing the abstraction
+3. Low level: implementation details for the API
+
+
+
+
+
+
+
+*/
